c9627d12357c1d4ee3b5fa346e8969a9a28ff776
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-01-03 16:15:37 +0200

    Add linters

--- script.js
+++ script.js
@@ -1,64 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   const textInput = document.getElementById('textInput');
   const popup = document.getElementById('popup');
   const heading1Btn = document.getElementById('heading1Btn');
   const expandableHeading1Btn = document.getElementById(
-    'expandableHeading1Btn'
+    'expandableHeading1Btn',
   );
   const typedText = document.getElementById('typedText');
 
   let isPopupVisible = false;
 
   // Handling the Popup Window
-  textInput.addEventListener('input', function(event) {
-    const inputValue = event.target.value;
-
-    if (inputValue.startsWith('/1')) {
-      showPopup();
-    } else {
-      hidePopup();
-    }
-  });
-
-  // Handling text enterd by the user
-  textInput.addEventListener('keyup', function(event) {
-    if (event.key === 'Enter') {
-      const inputValue = textInput.value.trim();
-
-      if (inputValue !== '' && inputValue !== '/1') {
-        appendTypedText(inputValue);
-        clearInput();
-        textInput.placeholder = 'Type / for blocks, @ to link docs or people';
-      }
-    }
-  });
-
-  // Handling the Popup Window options
-  heading1Btn.addEventListener('click', function() {
-    hidePopup();
-    clearInput();
-    textInput.placeholder = 'Heading 1';
-  });
-
-  expandableHeading1Btn.addEventListener('click', function() {
-    hidePopup();
-    clearInput();
-    textInput.placeholder = 'Expandable Heading 1';
-  });
-
-  //--------- Handling the shortcuts----------------//
-  document.addEventListener('keydown', function(event) {
-    let inputValue = textInput.value.trim();
-
-    if (inputValue === '#' && event.code === 'Space') {
-      clearInput();
-      textInput.placeholder = 'Heading 1';
-    } else if (inputValue === '>>#' && event.code === 'Space') {
-      clearInput();
-      textInput.placeholder = 'Expandable Heading 1';
-    }
-  });
-
   // Function to show the popup
   function showPopup() {
     if (!isPopupVisible) {
@@ -86,4 +37,54 @@ document.addEventListener('DOMContentLoaded', function() {
     newHeading.innerText = text;
     typedText.appendChild(newHeading);
   }
+
+  textInput.addEventListener('input', (event) => {
+    const inputValue = event.target.value;
+
+    if (inputValue.startsWith('/1')) {
+      showPopup();
+    } else if (isPopupVisible) {
+      clearInput();
+      hidePopup();
+    }
+  });
+
+  // Handling text enterd by the user
+  textInput.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      const inputValue = textInput.value.trim();
+
+      if (inputValue !== '' && inputValue !== '/1') {
+        appendTypedText(inputValue);
+        clearInput();
+        textInput.placeholder = 'Type / for blocks, @ to link docs or people';
+      }
+    }
+  });
+
+  // Handling the Popup Window options
+  heading1Btn.addEventListener('click', () => {
+    hidePopup();
+    clearInput();
+    textInput.placeholder = 'Heading 1';
+  });
+
+  expandableHeading1Btn.addEventListener('click', () => {
+    hidePopup();
+    clearInput();
+    textInput.placeholder = 'Expandable Heading 1';
+  });
+
+  // --------- Handling the shortcuts----------------//
+  document.addEventListener('keydown', (event) => {
+    const inputValue = textInput.value.trim();
+
+    if (inputValue === '#' && event.code === 'Space') {
+      clearInput();
+      textInput.placeholder = 'Heading 1';
+    } else if (inputValue === '>>#' && event.code === 'Space') {
+      clearInput();
+      textInput.placeholder = 'Expandable Heading 1';
+    }
+  });
 });
